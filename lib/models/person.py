b8c05cf65f244238d93cd17fc753015652875a47
Tejas Nampalli <TSNampalli@gmail.com>
2024-07-30 14:43:38 -0400

    person class init

---
+++ lib/models/person.py
@@ -0,0 +1,12 @@
+from models.__init__ import __init__
+
+class Person:
+    
+    all = []
+
+    def __init__(self, name, room, id):
+        self.name = name
+        self.room = room
+        self.id = id
+
+    
