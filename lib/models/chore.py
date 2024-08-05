ebabd9b3ab8e68bde1e9c3b2f42d06921a2bc06b
Tejas Nampalli <TSNampalli@gmail.com>
2024-08-05 14:31:10 -0400

    Validations

--- lib/models/chore.py
+++ lib/models/chore.py
@@ -15,10 +15,10 @@ class Chore:
 
     @task.setter
     def task(self, task):
-        if isinstance(task, str):
+        if isinstance(task, str) and 2 <= len(task) <= 22:
             self._task = task
         else:
-            raise ValueError("Task must be a string")   
+            raise ValueError("Task must be at least 2 characters and less than 22 characters long.")
 
     @property
     def status(self):
